07c933357ebe2f9143d71c3b88895e5c42a9fbc4
julesglad <91433872+julesglad@users.noreply.github.com>
2023-03-10 16:21:24 -0600

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { BasicFormComponent } from './basic-form/basic-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
